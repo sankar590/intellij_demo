b763e1ae7ee855eb5c63b929c86946aacc4fb365
sankarsan07 <bsankarsan365@gmail.com>
2025-02-24 15:43:24 +0530

    Pattern prog_1

---
+++ src/com/practice/numbers/PatternProg1.java
@@ -0,0 +1,12 @@
+package com.practice.numbers;
+
+public class PatternProg1 {
+    public static void main(String[] args) {
+        for (int i=1;i<=5;i++){
+            for( int j=i;j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
