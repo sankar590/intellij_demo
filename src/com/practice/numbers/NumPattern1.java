3975f3585c6fb30660cd255e577f952530904854
sankarsan07 <bsankarsan365@gmail.com>
2025-02-24 15:24:16 +0530

    Substring Program

---
+++ src/com/practice/numbers/NumPattern1.java
@@ -0,0 +1,8 @@
+package com.practice.numbers;
+
+public class NumPattern1 {
+    public static void main(String[] args) {
+        String s="java";
+        System.out.println(s.substring(1,3));
+    }
+}
