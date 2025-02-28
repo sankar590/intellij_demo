f00403ef9bd3dd865a5eff5def699db08f88f6a7
sankarsan07 <bsankarsan365@gmail.com>
2025-02-28 17:15:05 +0530

    String Lower case prog

--- src/com/practice/numbers/LowerCase.java
+++ src/com/practice/numbers/LowerCase.java
@@ -1,6 +1,6 @@
 package com.practice.numbers;
 
-public class LoweCase {
+public class LowerCase {
     public static void main(String[] args) {
         String s="SaNkar";
         System.out.println(s.toLowerCase());
