c76ac9b5baf47152bb0d642267decf516c9e10ec
sankarsan07 <bsankarsan365@gmail.com>
2025-02-28 12:02:25 +0530

    Test project updated

---
+++ src/com/practice/numbers/LoweCase.java
@@ -0,0 +1,8 @@
+package com.practice.numbers;
+
+public class LoweCase {
+    public static void main(String[] args) {
+        String s="SaNkar";
+        System.out.println(s.toLowerCase());
+    }
+}
--- src/com/practice/numbers/NumPattern1.java
+++ src/com/practice/numbers/NumPattern1.java
@@ -4,5 +4,6 @@ public class NumPattern1 {
     public static void main(String[] args) {
         String s="java";
         System.out.println(s.substring(1,3));
+        System.out.println(s.toUpperCase());
     }
 }
